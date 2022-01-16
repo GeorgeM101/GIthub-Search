a47c36e8dfcb6c17c62f238be201d9895e00ea7f
GeorgeM101 <georgmboya@gmail.com>
2022-01-16 23:58:06 +0300

    added search service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  APIkey:'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'
 };
 
 /*
